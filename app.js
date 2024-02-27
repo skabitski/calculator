8d4b6bb7d8bd101fa36c3cddb840ce454f709871
Maks_Skabitski <mskabitskiy@gmail.com>
2024-02-27 19:52:54 +0200

    finish

---
+++ app.js
@@ -0,0 +1,20 @@
+const calc = document.querySelector(".calc");
+const result = document.querySelector("#result");
+calc.addEventListener("click", function (event) {
+  if (!event.target.classList.contains("calc_btn")) return; //iskluchenie lishnih elementov
+
+  const value = event.target.innerText;
+
+  switch (value) {
+    case "C":
+      result.innerText = "";
+      break;
+
+    case "=":
+      result.innerText = eval(result.innerText).toFixed(2);
+      break;
+
+    default:
+      result.innerText += value;
+  }
+});
